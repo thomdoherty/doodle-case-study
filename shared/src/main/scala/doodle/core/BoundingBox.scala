cad19dacf067984564407bf465922893de242eb1
Tom Doherty <thomdoherty@yahoo.com>
2015-10-21 15:33:26 +0100

    Implemented bounding box further

--- shared/src/main/scala/doodle/core/BoundingBox.scala
+++ shared/src/main/scala/doodle/core/BoundingBox.scala
@@ -2,7 +2,10 @@ package doodle.core
 
 // A local coordinate system to draw within
 
-final case class BoundingBox(leftCoordinate: Double,  rightCoordinate: Double, topCoordinate: Double, bottomCoordinate: Double) {
+final case class BoundingBox(leftCoordinate: Double,
+    rightCoordinate: Double,
+    topCoordinate: Double,
+    bottomCoordinate: Double) {
 
   // Want to calculate width and height from coordinates
   // Need to figure out centre of boundingbox?
@@ -10,12 +13,39 @@ final case class BoundingBox(leftCoordinate: Double,  rightCoordinate: Double, t
     val height: Double =  topCoordinate - bottomCoordinate
     val width: Double =  rightCoordinate - leftCoordinate
     
+    def above(that: BoundingBox): BoundingBox =
+       BoundingBox(
+          this.leftCoordinate min that.leftCoordinate,
+          (this.height + that.height) / 2,
+          this.rightCoordinate max that.rightCoordinate,
+          -(this.height + that.height) / 2)
+          
+    def beside(that: BoundingBox): BoundingBox =
+      BoundingBox(
+          -(this.width + that.width) / 2,
+          this.topCoordinate max that.topCoordinate,
+          (this.width + that.width) / 2,
+          this.bottomCoordinate min that.bottomCoordinate)
+          
+    def on(that: BoundingBox): BoundingBox =
+       BoundingBox(
+          this.leftCoordinate min that.leftCoordinate,
+          this.topCoordinate max that.topCoordinate,
+          this.rightCoordinate max that.rightCoordinate,
+          this.bottomCoordinate min that.bottomCoordinate)
+          
+}
+    
     //Need apply method overridden to handle image => need companion object
-    object BoundingBox{
+   /* object BoundingBox{
       
     def apply(image: Image): BoundingBox = image match {    
       case Rectangle(width, height) =>
         BoundingBox(-width/2, height/2, width/2, -height/2)
+      case Circle(r) => BoundingBox(-r, r, r, -r)
+      case Rectangle(w, h) => BoundingBox(-w/2, h/2, w/2, -h/2)
+      case Above(a, b) => a.this above b.this
+      case On(o, u) => o.boundingBox on u.boundingBox
+      case Beside(l, r) => l.boundingBox beside r.boundingBox
       }  
-   }
-}
+   }*/
--- shared/src/main/scala/doodle/core/Image.scala
+++ shared/src/main/scala/doodle/core/Image.scala
@@ -1,6 +1,18 @@
 package doodle.core
 
+import doodle.backend.Canvas
+
 sealed trait Image {
+  
+  val boundingBox: BoundingBox = this match {    
+      //case Rectangle(width, height) => BoundingBox(-width/2, height/2, width/2, -height/2)
+      case Circle(r) => BoundingBox(-r, r, r, -r)
+      case Rectangle(w, h) => BoundingBox(-w/2, h/2, w/2, -h/2)
+      case Above(a, b) => a.boundingBox above b.boundingBox
+      case On(o, u) => o.boundingBox on u.boundingBox
+      case Beside(l, r) => l.boundingBox beside r.boundingBox
+  }
+  
   def above(below: Image): Image =
     Above(this, below)
     
@@ -13,20 +25,28 @@ sealed trait Image {
   def on(bottom: Image): Image =
     On(this, bottom)
 
-  def draw(canvas: Canvas): Unit = {
+  /*def draw(canvas: Canvas): Unit = {
     // structural recursion
     // Set size of canvas based on bounding box coordinates
     // Then draw image on canvas
     val boundingBox: BoundingBox = BoundingBox(this) //This means we need apply method in bounding box that takes image
-    draw(canvas, boundingBox.width, boundingBox.height)
-  }
+    draw(canvas, boundingBox.width, boundingBox.height)    
+  }*/
+  
+  def draw(canvas: Canvas): Unit = this match {
+     case Circle(r) => canvas.circle(0.0, 0.0, r)
+     case Rectangle(w,h) => canvas.rectangle(-w/2, h/2, w/2, -h/2)
+     case Above(a, b) => a.draw(canvas); b.draw(canvas)
+     case On(o, u) => o.draw(canvas); u.draw(canvas)
+     case Beside(l, r) => l.draw(canvas); r.draw(canvas)
+     }
 
   // A helper method you will probably want
-  def draw(canvas: Canvas, originX: Double, originY: Double): Unit = {
+  /*def draw(canvas: Canvas, originX: Double, originY: Double): Unit = {
     canvas.setSize(width, height)
     canvas.setOrigin(origin.x.ceil.toInt, origin.y.ceil.toInt)
     //Draw?
-   }
+   }*/
 }
 final case class Circle(radius: Double) extends Image
 final case class Rectangle(width: Double, height: Double) extends Image
